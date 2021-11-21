0e73929c136c532962f24d98d5d29989c6a27c5a
Катя <katya.dendisol@gmail.com>
2021-11-21 18:57:26 +0300

    MainActivityTest

---
+++ app/src/test/java/com/example/enwords/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.enwords;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.Test
+    public void onCreate() {
+        assertEquals(4, 2+2);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void onButtonClickStart() {
+         assertEquals(7, 5+2);
+    }
+}
